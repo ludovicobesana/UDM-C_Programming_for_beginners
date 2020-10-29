e1dedaf493ef2e75b0dca1b5faed9a579d7590d9
Ludovico <ludovicobesanaguide@gmail.com>
2020-10-29 01:14:13 +0100

    🎉 FEATURE: Added first exercise Hello world in c

---
+++ exercises/HelloWorld.c
@@ -0,0 +1,13 @@
+/*========================================
+ * Name: HelloWorld.c
+ * Author: Ludovico Besana
+ * Version: 1.0
+ * Description: Simple Hello World in c
+ *======================================*/
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello World!");
+    return 0;
+}
