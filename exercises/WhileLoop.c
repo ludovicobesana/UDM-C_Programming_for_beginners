10d19c90f0c4e5c86cd983cda29149e395995a4b
Ludovico <ludovicobesanaguide@gmail.com>
2020-11-01 20:29:11 +0100

    🎉 FEATURE: Added eighth exercise while loop in c

---
+++ exercises/WhileLoop.c
@@ -0,0 +1,19 @@
+/*================================
+ * Name: WhileLoop.c
+ * Author: Ludovico Besana
+ * Version: 1.0
+ * Description: While loop in c
+ *==============================*/
+
+#include <stdio.h>
+
+int main() {
+  
+  int i = 1;
+  while(i <= 5) {
+    printf("Dennis Ritchie, %d\n", i);
+    i++;
+  }
+  
+  return 0;
+}
