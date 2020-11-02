fc60eac195211ac6441c833877e15494a9b073bb
Ludovico <ludovicobesanaguide@gmail.com>
2020-11-02 01:24:30 +0100

    🎉 FEATURE: Added tenth exercise for loop in c

---
+++ exercises/ForLoop.c
@@ -0,0 +1,17 @@
+/*================================
+ * Name: ForLoop.c
+ * Author: Ludovico Besana
+ * Version: 1.0
+ * Description: for loop in c
+ *==============================*/
+
+#include <stdio.h>
+
+int main() {
+  int i;
+
+  for(i=1; i<=5; i++) {
+    printf("Dennis Ritchie, %d\n", i);
+  } 
+  return 0;
+}
